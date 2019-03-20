6ab8ba724aded7da36d83c5491728bfff8fecfd5
Dustin Snoap <afrobacon@gmail.com>
2019-03-19 22:42:03 -0400

    Create a rough sketch of a sub header

--- template0/src/App.js
+++ template0/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './comps/header/header'
+import SubHeader from './comps/subHeader/subheader'
 import './reset.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <SubHeader />
       </div>
     );
   }
---
+++ template0/src/comps/subHeader/subheader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './subheader.css'
+
+const SubHeader = () =>
+    <div className='subheader'>
+        <div className='container'>
+            <div className='group'>
+                <pre>VIEW</pre>
+                <button>v1</button>
+                <button>v2</button>
+                <button>v3</button>
+                <button>Acnt</button>
+            </div>
+            <div className='group'>
+                <pre>SORT</pre>
+                <pre>type \./</pre>
+            </div>
+            <div className='group'>
+                <pre>TOPIC</pre>
+                <pre>topic name \./</pre>
+            </div>
+            <div className='group'>
+                <pre>SUB-TOPIC</pre>
+                <pre>topic name \./</pre>
+            </div>
+        </div>
+    </div>
+
+export default SubHeader
